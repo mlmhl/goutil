9c2c9c13829e8feeb61d65d694bfa1ba50b61132
luffyma(马林) <luffyma@tencent.com>
2016-06-24 09:10:04 +0800

    add math utility

---
+++ math/dim.go
@@ -0,0 +1,33 @@
+package math
+
+func MaxInt64(x, y int64) int64 {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func MinInt64(x, y int64) int64 {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func MaxInt(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func MinInt(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
